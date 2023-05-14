dbf596b4450422c69c05ed8a34feb0de2482263f
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-05-15 00:28:58 +0300

    "done"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
  import { ToastContainer} from 'react-toastify';
 import {AppDiv} from './App.styled'
-import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 
 export class App extends Component  {
@@ -10,18 +9,12 @@ export class App extends Component  {
     searchQuery:''
   };
 
-  
-
-  onFormData = (value) => {
-    this.setState({ searchQuery: value });
-  }
-  
   render() {
     const { searchQuery } = this.state;
     return (
        
       <AppDiv>
-        <Searchbar onFormData={this.onFormData}/>  
+
          <ToastContainer/>
         <ImageGallery searchQuery={searchQuery} />
     </AppDiv>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,88 +7,65 @@ import {PixabayApi} from '../Services/PixabayApi';
 import { Loader } from 'components/Loader';
 import { Button } from 'components/Button';
 import PropTypes from 'prop-types';
-
+import { Searchbar } from 'components/Searchbar';
 
 export  class ImageGallery extends Component {
     state = {
-        searchSet: null,
-        renderList:[],
+        finishRenderList: null,
         error: null,
         page: 1,
         status: 'idle',
-       
+        query: '',
     }
-    
-   async componentDidUpdate(prevProps, prevState) {
-        const prevName = prevProps.searchQuery.search;
-        const nextName = this.props.searchQuery.search;
+    async componentDidUpdate(prevProps, prevState) {
+        const prevQuery = prevState.query;
         const prevPage = prevState.page;
-        const nextPage = this.state.page;
-       let reciveSetPIcture = null;
-       
-       
-       
-       if (prevName !== nextName || prevPage !== nextPage) {
-           try {
-            this.setState({status:'pending'});
-             reciveSetPIcture = await PixabayApi(nextName, nextPage);
-                if (reciveSetPIcture.hits.length === 0) {
-                    toast.error(`there is no  query with  "${nextName}"`, {
-                    position: "top-right",
-                    autoClose: 2000,
-                    theme: "colored",
-                    });
-                     this.setState({ status: 'reject', searchSet: [] }); 
-                     return;
-                 } 
-                 this.setState({ status: 'resolved', searchSet: reciveSetPIcture.hits }) 
-              this.setState(prevState => ({renderList:[...prevState.renderList, ...reciveSetPIcture.hits]}));
-      
-            } catch(error) {
-                    toast.error(`"${error}" Something were wrong `, {
+        const {page, query} = this.state;
+
+        if (prevQuery !== query || prevPage !== page) {
+
+            this.FetchingSevrice(query, page);
+        }
+    };  
+    FetchingSevrice = async (searchWord, page) => {
+        try {
+            this.setState({ status: 'pending' });    
+            const reciveSetPIcture = await PixabayApi(searchWord, page); 
+            if (reciveSetPIcture.total === 0) {
+                                toast.error(`there is no  query with  "${searchWord}"`, {
+                                position: "top-right",
+                                autoClose: 2000,
+                                theme: "colored",
+                                });
+                                this.setState({ status: 'reject'}); 
+                                return;
+                        } 
+                        this.setState(prevState => ({ finishRenderList:[...prevState.finishRenderList, ...reciveSetPIcture.hits],status: 'resolved'})); 
+            }catch(error) {
+                toast.error(`"${error}" Something were wrong `, {
                     position: "top-right",
                     autoClose: 3000,
                     theme: "colored",
-                    });
+                });       
             }
-       }  
-       
-       if (prevName !== nextName && nextPage !== 1) {
-           this.setState({ renderList: [], page: 1 });
-       }
-        }
-
+    }
+    onFormData = (value) => {
+        this.setState({ query: value.search, page: 1, finishRenderList: [] });
+    }    
     onLoadMore = () => {
         this.setState(prevState => ({ page: prevState.page + 1  }));
-     
     };
-    
-  
     render() {
-        const { error, status, renderList } = this.state;
-      
-        if (status === 'idle') {
-            return <div><H1>Please make a request and press enter </H1></div>;
-        }
-
-        if (status === 'pending') {
-            return (
-                <Loader />
-            );
-        }
-
-        if (status === 'rejected') {
-            return (<> <h1> {error.message}</h1>
-           
-            </> );
-        }
-
-        if (status === 'resolved') {
-             return (
+        const { error, status, finishRenderList } = this.state;
+        return (
             <>
-                {renderList &&
-                    <Ul> 
-                        {renderList.map(({ id, webformatURL, tags, largeImageURL } ) => (
+                <Searchbar onFormData={this.onFormData} />  
+              {(status === 'idle') && (<div><H1>Please make a request and press enter </H1></div>)}  
+              { (status === 'pending') &&  <Loader /> }
+            {(status === 'rejected') && (<h1> {error.message} </h1>)}
+                {(status === 'resolved' && finishRenderList) &&  (<>
+                   <Ul> 
+                        {finishRenderList.map(({ id, webformatURL, tags, largeImageURL } ) => (
                             <ImageGalleryItem
                                 key={id}
                                 webformatURL={webformatURL}
@@ -96,15 +73,19 @@ export  class ImageGallery extends Component {
                                 largeImageURL={largeImageURL}
                             />
                         ))} 
-                    </Ul >
-                    }
-                <Button onClick={this.onLoadMore} />
-            </>       
+                    </Ul >  
+                    <Button onClick={this.onLoadMore} />
+                    </>
+                )     
+            }
+            </>
             );
-        }
     };
     };
 
+
+
 ImageGallery.propTypes = {
-     prevName: PropTypes.string  
-    }
+    reciveSetPIcture: PropTypes.objectOf(PropTypes.object)
+    
+};
